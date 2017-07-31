9f9cdc7d7b79d4da1e240c593a96d077bfaf6c25
jielong.lin <493164984@qq.com>
2017-07-31 20:03:00 +0800

    Commited by using jll.Git.sh @ 2017-07-31 20:03:00
    Changes as follows:
    R  git_pull_to_git_repository_with_key.sh -> Abandon/git_pull_to_git_repository_with_key.sh
    R  git_push_to_git_repository_with_key.sh -> Abandon/git_push_to_git_repository_with_key.sh
    R  vicc_installer.tpv.sh.abandon -> Abandon/vicc_installer.tpv.sh.abandon
    R  release -> README
    M  vicc_installer_over_linux64.sh

--- vicc_installer_over_linux64.sh
+++ vicc_installer_over_linux64.sh
@@ -11,44 +11,56 @@ else
     selfPath=$(cd ${selfPath}; pwd)
 fi
 
+relPKG="${HOME}/.vicc/${JLLCFG_gitURL##*/}"
+if [ -e "${relPKG}" ]; then
+    echo -e "JLL@Installer| [32mRemoving original vicc IDE under ${relPKG}[0m"
+    [ -e "${relPKG}/jllsystem/settings/installer_with_sync.sh" ] && \
+        ${relPKG}/jllsystem/settings/installer_with_sync.sh "uninstall"
+    rm -rf ${relPKG}
+fi
+relPKG="${relPKG%\.git}"
+if [ -e "${relPKG}" ]; then
+    echo -e "JLL@Installer| [32mRemoving original vicc IDE under ${relPKG}[0m"
+    [ -e "${relPKG}/jllsystem/settings/installer_with_sync.sh" ] && \
+        ${relPKG}/jllsystem/settings/installer_with_sync.sh "uninstall"
+    rm -rf ${relPKG}
+fi
+
+if [ ! -e "${HOME}/.vicc" ]; then
+    echo -e "JLL@Installer| [32mCreating ${HOME}/.vicc[0m"
+    mkdir -p ${HOME}/.vicc
+fi
+
 #####
 #####  MAIN
 #####
 while [ 1 -eq 1 ]; do
-    _DT="$(date +%Y%m%d%H%M)"
-    _relPKG="${HOME}/.vicc/release_R${_DT}"
-    if [ ! -e "${_relPKG}" ]; then
-        mkdir -p ${_relPKG}
-    fi
-    cd ${_relPKG}
-    echo -e \
-        "JLL@Installer| ${Fgreen}git clone ${JLLCFG_gitURL}${AC} under ${Fgreen}${_relPKG}${AC}"
+
+    cd ${HOME}/.vicc
+    echo -e "JLL@Installer| [32mgit clone ${JLLCFG_gitURL} under ${HOME}/.vicc[0m"
     git clone ${JLLCFG_gitURL}
     cd - >/dev/null
-    _releasePKG="${_relPKG}/${JLLCFG_gitURL##*/}"
-    echo "JLL@Installer| Probe.1 to finding if .git is under ${_releasePKG}"
-    if [ ! -e "${_releasePKG}/.git" ]; then
-        _releasePKG="${_releasePKG%\.git}"
-        echo "JLL@Installer| Probe.2 to finding if .git is under ${_releasePKG}"
-    fi
-    if [ ! -e "${_releasePKG}/.git" ]; then
-        echo "JLL@Installer| Not found the legal release package path from ${_releasePKG}"
+
+    relPKG="${HOME}/.vicc/${JLLCFG_gitURL##*/}/jllsystem/settings"
+    echo "JLL@Installer| Probe.1 to finding if installer_with_sync.sh is under ${relPKG}"
+    if [ -e "${relPKG}/installer_with_sync.sh" ]; then
+        echo -e "JLL@Installer| [32mInstalling latest vicc IDE under ${relPKG}[0m"
+        ${relPKG}/installer_with_sync.sh "install"
         break
     fi
-    echo "JLL@Installer| Obtained release package path=${_releasePKG}"
-    if [ ! -e "${_releasePKG}/jllsystem/settings/installer_with_sync.sh" ]; then
-        echo "JLL@Installer| not found ${_releasePKG}/jllsystem/settings/installer_with_sync.sh"
+    relPKG="${HOME}/.vicc/${JLLCFG_gitURL##*/}"
+    relPKG="${relPKG%\.git}/jllsystem/settings"
+    echo "JLL@Installer| Probe.2 to finding if installer_with_sync.sh is under ${relPKG}"
+    if [ -e "${relPKG}/installer_with_sync.sh" ]; then
+        echo -e "JLL@Installer| [32mInstalling latest vicc IDE under ${relPKG}[0m"
+        ${relPKG}/installer_with_sync.sh "install"
         break
     fi
-    cd ${_releasePKG}/jllsystem/settings
-    ./installer_with_sync.sh "install"
-    cd - >/dev/null
-    [ -e "${_releasePKG}/README" ] && cp -rf ${_releasePKG}/README ${selfPath} 
     break
 done
+[ -e "${relPKG}/README" ] && cp -rf ${relPKG}/README ${selfPath}
 
-[ x"${_releasePKG}" != x ]  && unset _releasePKG
-[ x"${_relPKG}" != x ]  && unset _relPKG
+[ x"${relPKG}" != x ]  && unset relPKG
 [ x"${selfPath}" != x ]  && unset selfPath
-
+[ x"${JLLCFG_gitURL}" != x ]  && unset JLLCFG_gitURL 
 
